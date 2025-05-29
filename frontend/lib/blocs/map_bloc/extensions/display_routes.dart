8e6ecfacfd4b4be4e171b3447edb087536a20f63
Eleni Kehrioti <elenikehrioti@gmail.com>
2025-05-29 04:39:21 +0300

    fixed display

--- frontend/lib/blocs/map_bloc/extensions/display_routes.dart
+++ frontend/lib/blocs/map_bloc/extensions/display_routes.dart
@@ -1,8 +1,10 @@
 part of '../map_bloc.dart';
 
 extension MapBlocDisplay on MapBloc {
-  Future<Map<String, dynamic>?> _fetchRoute(MapboxFeature feature, bool alternatives) async {
-
+  Future<Map<String, dynamic>?> _fetchRoute(
+    MapboxFeature feature,
+    bool alternatives,
+  ) async {
     if (feature == null) {
       print("Attempted to navigate but feature was null.");
       return null;
@@ -12,7 +14,6 @@ extension MapBlocDisplay on MapBloc {
       final position = await geolocator.Geolocator.getCurrentPosition(
         desiredAccuracy: geolocator.LocationAccuracy.high,
       ).timeout(const Duration(seconds: 10));
-
 
       // Call the API with `alternatives` query param
       final responseJson = await mapService.getRoutesJson(
@@ -29,7 +30,6 @@ extension MapBlocDisplay on MapBloc {
         final List<List<List<double>>> alternativeRoutes = [];
 
         final routes = responseJson['routes'] as List<dynamic>?;
-
 
         if (routes != null) {
           for (var route in routes) {
@@ -53,28 +53,31 @@ extension MapBlocDisplay on MapBloc {
       print("Navigation error: $e");
     }
     return null;
-
   }
 
-  Map<dynamic, dynamic>? getRoute(dynamic routeObject){
-    if (routeObject == null || routeObject['coordinates'] == null || routeObject['coordinates'] is! List) {
+  Map<dynamic, dynamic>? getRoute(dynamic routeObject) {
+    if (routeObject == null ||
+        routeObject['coordinates'] == null ||
+        routeObject['coordinates'] is! List) {
       emit(state.copyWith(errorMessageGetter: () => 'Route data is invalid'));
       return null;
     }
 
     final coordinates = routeObject['coordinates'] as List;
 
-    final fixedLineCoordinates = coordinates.map<List<double>>((c) {
-      if (c is List && c.length >= 2) {
-        return [c[1].toDouble(), c[0].toDouble()]; // lat, lng για Mapbox
-      } else {
-        throw Exception('Invalid coordinate format');
-      }
-    }).toList();
+    final fixedLineCoordinates =
+        coordinates.map<List<double>>((c) {
+          if (c is List && c.length >= 2) {
+            return [c[1].toDouble(), c[0].toDouble()]; // lat, lng για Mapbox
+          } else {
+            throw Exception('Invalid coordinate format');
+          }
+        }).toList();
 
     // === Extract instructions from steps ===
     final instructionsList = routeObject['instructions'];
-
+    final accessibilityScore = routeObject['accessibilityScore'];
+    final colorHex = routeObject['color'];
     final List<NavigationStep> routeSteps = [];
 
     if (instructionsList is List) {
@@ -87,14 +90,21 @@ extension MapBlocDisplay on MapBloc {
     return {
       'coordinates': fixedLineCoordinates,
       'routeSteps': routeSteps,
+      'accessibilityScore': accessibilityScore,
+      'color': colorHex,
     };
   }
 
-  Future<void> _displayRoute(Map<String, dynamic> responseJson, Emitter<MapState> emit) async {
+  Future<void> _displayRoute(
+    Map<String, dynamic> responseJson,
+    Emitter<MapState> emit,
+  ) async {
     try {
       final map = state.mapController;
       if (map == null) {
-        emit(state.copyWith(errorMessageGetter: () => 'Map controller not ready'));
+        emit(
+          state.copyWith(errorMessageGetter: () => 'Map controller not ready'),
+        );
         return;
       }
 
@@ -102,86 +112,104 @@ extension MapBlocDisplay on MapBloc {
       final route = getRoute(routeObject);
       final List<NavigationStep> routeSteps = route!['routeSteps'];
       final fixedLineCoordinates = route['coordinates'];
-      add(RemoveAlternativeRoutes());
-      await _addLine(fixedLineCoordinates, 0);
+      final accessibilityScore = route['accessibilityScore'];
+      final colorHex = route['color'];
+      await _remove();
+      await _addLine(fixedLineCoordinates, 0, null);
 
       print(routeSteps);
-      emit(state.copyWith(
-        errorMessageGetter: () => null,
-        routeSteps: routeSteps,
-      ));
+      emit(
+        state.copyWith(errorMessageGetter: () => null, routeSteps: routeSteps),
+      );
     } catch (e) {
-      emit(state.copyWith(errorMessageGetter: () => 'Error displaying route: $e'));
+      emit(
+        state.copyWith(errorMessageGetter: () => 'Error displaying route: $e'),
+      );
     }
-
   }
 
-  Future<void> _onDisplayAlternativeRoutes(DisplayAlternativeRoutes event, Emitter<MapState> emit,) async {
+  Future<void> _onDisplayAlternativeRoutes(
+    DisplayAlternativeRoutes event,
+    Emitter<MapState> emit,
+  ) async {
     try {
       final map = state.mapController;
       if (map == null) {
-        emit(state.copyWith(errorMessageGetter: () => 'Map controller not ready'));
+        emit(
+          state.copyWith(errorMessageGetter: () => 'Map controller not ready'),
+        );
         return;
       }
       final route = await _fetchRoute(event.feature, true);
       final routes = route!['routes'] as List<dynamic>?;
       var alternativeRoutes = [];
-      add(RemoveAlternativeRoutes());
+      await _remove();
 
       for (int i = 0; i < routes!.length; i++) {
         final route = routes[i];
         final r = getRoute(route);
         final coordinates = r!['coordinates'];
         final List<NavigationStep> routeSteps = r['routeSteps'];
-        await _addLine(coordinates, i);
+        final accessibilityScore = r['accessibilityScore'];
+        await _addLine(coordinates, i, accessibilityScore);
         alternativeRoutes.add(r);
       }
-      emit(state.copyWith(errorMessageGetter: () => null, alternativeRoutes: alternativeRoutes));
+      emit(
+        state.copyWith(
+          errorMessageGetter: () => null,
+          alternativeRoutes: alternativeRoutes,
+        ),
+      );
     } catch (e) {
-      emit(state.copyWith(errorMessageGetter: () => 'Error displaying alternative routes: $e'));
+      emit(
+        state.copyWith(
+          errorMessageGetter: () => 'Error displaying alternative routes: $e',
+        ),
+      );
     }
   }
 
-  Future<void> _addLine(List<dynamic> fixedRoute, int i) async {
-    final style = state.mapController?.style;
+  Future<void> _addLine(
+        List<dynamic> fixedRoute,
+        int i,
+        double? accessibilityScore,
+        ) async {
+      final style = state.mapController?.style;
 
-    final sourceId = 'alt-route-source-$i';
-    final layerId = 'alt-route-layer-$i';
+      final sourceId = 'alt-route-source-$i';
+      final layerId = 'alt-route-layer-$i';
+      print("Adding line with color: $accessibilityScore");
+      final List<int> routeColors = [
+        Colors.blue.value,
+        Colors.green.value,
+        Colors.red.value,
+        Colors.orange.value,
+        Colors.purple.value,
+      ];
 
-    final List<int> routeColors = [
-      Colors.blue.value,
-      Colors.green.value,
-      Colors.red.value,
-      Colors.orange.value,
-      Colors.purple.value,
-    ];
-
-    final geojson = {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "LineString",
-            "coordinates": fixedRoute,
+      fixedRoute = fixedRoute.map((coord) => [coord[1], coord[0]]).toList();
+      final geojson = {
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "geometry": {"type": "LineString", "coordinates": fixedRoute},
+            "properties": {},
           },
-          "properties": {}
-        }
-      ]
-    };
+        ],
+      };
 
-    await style?.addSource(
-      mapbox.GeoJsonSource(
-        id: sourceId,
-        data: jsonEncode(geojson),
-      ),
+      print(fixedRoute);
+
+      await style?.addSource(
+      mapbox.GeoJsonSource(id: sourceId, data: jsonEncode(geojson)),
     );
-
+    print(makeColor(accessibilityScore!).toARGB32());
     await style?.addLayer(
       mapbox.LineLayer(
         id: layerId,
         sourceId: sourceId,
-        lineColor: routeColors[i % routeColors.length],
+        lineColor: makeColor(accessibilityScore!).toARGB32(),
         lineWidth: 4.0,
         lineJoin: mapbox.LineJoin.ROUND,
         lineCap: mapbox.LineCap.ROUND,
@@ -189,7 +217,14 @@ extension MapBlocDisplay on MapBloc {
     );
   }
 
-  Future<void> _onRemoveAlternativeRoutes(RemoveAlternativeRoutes event, Emitter<MapState> emit) async {
+  Future<void> _onRemoveAlternativeRoutes(
+    RemoveAlternativeRoutes event,
+    Emitter<MapState> emit,
+  ) async {
+    await _remove();
+  }
+
+  Future<void> _remove() async {
     final style = state.mapController?.style;
     if (style == null) return;
 
@@ -200,11 +235,13 @@ extension MapBlocDisplay on MapBloc {
       bool removedAnything = false;
 
       if ((await style.styleLayerExists(layerId))) {
+        print("Removing layer: $layerId");
         await style.removeStyleLayer(layerId);
         removedAnything = true;
       }
 
       if ((await style.styleSourceExists(sourceId))) {
+        print("Removing source: $sourceId");
         await style.removeStyleSource(sourceId);
         removedAnything = true;
       }
@@ -212,5 +249,13 @@ extension MapBlocDisplay on MapBloc {
       if (!removedAnything) break;
       i++;
     }
+  }
+
+  Color makeColor(double accessibilityScore) {
+    if (accessibilityScore == null) return Colors.blue;
+    if (accessibilityScore < 0.4) return Colors.red;
+    if (accessibilityScore < 0.7) return Colors.yellow;
+    if (accessibilityScore >= 0.7) return Colors.green;
+    return Colors.red;
   }
 }
